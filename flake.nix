10784891a12da8947edef01ddc5450585914d712
AsciiMoth <ascii@moth.contact>
2025-06-24 03:17:10 +0400

    Add direnv and flake dev environments

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Web buisness cards service";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+          };
+        };
+      };
+    in {
+      nixosModule = import ./module.nix;
+      devShell = pkgs.mkShell {
+        inherit (checks.pre-commit-check) shellHook;
+        buildInputs = [
+          pkgs.go
+        ];
+      };
+    });
+}
