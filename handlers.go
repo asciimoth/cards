8b41d436574256a13db2fcdbf57616af6e0271bd
AsciiMoth <ascii@moth.contact>
2025-06-24 05:20:25 +0400

    Add user session management

--- handlers.go
+++ handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -20,7 +21,43 @@ func errorPage(c *gin.Context, status int, text string) {
 	})
 }
 
+func sessionMiddleware(log *logrus.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("User", nil)
+		user := User{
+			ID:         0,
+			ProviderID: "",
+			Name:       "",
+		}
+		sess := sessions.Default(c)
+		str_uid := sess.Get("user_id")
+		if str_uid != nil {
+			uid, err := strconv.ParseUint(str_uid.(string), 10, 64)
+			if err != nil {
+				log.Error("Request with broken uid; failed to convert to uint")
+				c.AbortWithStatus(http.StatusBadRequest)
+				return
+			}
+			user.ID = uint(uid)
+		} else {
+			return
+		}
+		c.Set("User", &user)
+		c.Next()
+	}
+}
+
+func getUser(c *gin.Context) *User {
+	user := c.MustGet("User")
+	if user == nil {
+		return nil
+	}
+	return user.(*User)
+}
+
 func SetupRoutes(g *gin.Engine, ctx context.Context, storage *BlobStorage, db *PGDB, log *logrus.Logger, providers []string) {
+	g.Use(sessionMiddleware(log))
+
 	g.NoRoute(func(c *gin.Context) {
 		errorPage(c, http.StatusNotFound, "")
 	})
@@ -28,23 +65,23 @@ func SetupRoutes(g *gin.Engine, ctx context.Context, storage *BlobStorage, db *P
 	g.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
 			"Title": "Main page",
-			//"User":  getUser(c),
+			"User":  getUser(c),
 		})
 	})
 	g.GET("/faq", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "faq.html", gin.H{
 			"Title": "Faq",
-			//"User":  getUser(c),
+			"User":  getUser(c),
 		})
 	})
 	g.GET("/tutorial", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "tutorial.html", gin.H{
 			"Title": "Tutorial",
-			//"User":  getUser(c),
+			"User":  getUser(c),
 		})
 	})
 
-	// OAuth related headers
+	// OAuth related handlers
 	{
 		oauth := g.Group("/")
 		oauth.GET("/auth/:provider", func(c *gin.Context) {
@@ -92,6 +129,24 @@ func SetupRoutes(g *gin.Engine, ctx context.Context, storage *BlobStorage, db *P
 			sess.Save()
 
 			c.Redirect(http.StatusTemporaryRedirect, "/cards")
+		})
+	}
+
+	// User session management handlers
+	{
+		us := g.Group("/")
+		us.GET("/login", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "login.html", gin.H{
+				"Title":     "Login",
+				"Providers": providers,
+				"User":      getUser(c),
+			})
+		})
+		us.GET("/logout", func(c *gin.Context) {
+			sess := sessions.Default(c)
+			sess.Clear()
+			sess.Save()
+			c.Redirect(http.StatusTemporaryRedirect, "/")
 		})
 	}
 }
