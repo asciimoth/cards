383f6b830a78e34166a59aec2a9f2e8a8ae3df26
AsciiMoth <ascii@moth.contact>
2025-06-24 04:25:50 +0400

    Add server setup

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
+import (
+	"context"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+func getStopCtx() (context.Context, context.CancelFunc) {
+	return signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+}
+
 func main() {
 	log := SetupLogger()
+
 	storage := SetupBlobStorage(log)
-	_ = storage
-	log.Debug("S3 storage connected")
 	db := SetupDB(storage, log)
+	r, srv := SetupServer(log)
+
+	ctx, stop := getStopCtx()
+	defer stop()
+
+	var wg sync.WaitGroup
+	RunServer(srv, &wg, ctx, log)
+
+	<-ctx.Done()
+	log.Info("Shutdown signal received")
+	wg.Wait()
+
+	log.Info("All groutines are finished")
+
 	_ = db
-	log.Debug("DB connected")
+	_ = r
 }
---
+++ server.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"embed"
+	"fmt"
+	"html/template"
+	"io/fs"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+)
+
+//go:embed templates/*.html
+var templatesFS embed.FS
+
+//go:embed static/*
+var staticFS embed.FS
+
+func SetupServer(log *logrus.Logger) (*gin.Engine, *http.Server) {
+	str_ttl := os.Getenv("COOKIE_TTL")
+	cookie_ttl := 24
+	if str_ttl == "" {
+		log.Warnf("Cookies TTL not set; using default value %d hours", cookie_ttl)
+	} else {
+		ttl, err := strconv.Atoi(str_ttl)
+		cookie_ttl = ttl
+		if err != nil {
+			log.Fatalf("Failed to parse cooie ttl: %s", str_ttl)
+		}
+	}
+
+	host, port := os.Getenv("GIN_HOST"), os.Getenv("GIN_PORT")
+	if host == "" {
+		host = "0.0.0.0"
+	}
+	if port == "" {
+		port = "8080"
+	}
+	addr := fmt.Sprintf("%s:%s", host, port)
+
+	log.WithFields(logrus.Fields{
+		"addr": addr,
+	}).Debug("Setting up server")
+
+	gin.DefaultWriter = log.WriterLevel(logrus.InfoLevel)
+	gin.DefaultErrorWriter = log.WriterLevel(logrus.ErrorLevel)
+
+	gin.SetMode(gin.ReleaseMode)
+
+	g := gin.New()
+
+	g.Use(gin.LoggerWithWriter(log.Writer()))
+	g.Use(gin.RecoveryWithWriter(log.Writer()))
+
+	// Serve static files from embedded FS
+	staticContent, _ := fs.Sub(staticFS, "static")
+	g.StaticFS("/static", http.FS(staticContent))
+
+	// Parse and set HTML templates
+	tmpl := template.Must(
+		template.New("*").Funcs(template.FuncMap{}).
+			ParseFS(templatesFS, "templates/*.html"),
+	)
+	g.SetHTMLTemplate(tmpl)
+
+	store := cookie.NewStore([]byte(os.Getenv("SESSION_SECRET")))
+	store.Options(sessions.Options{
+		Path:     "/",
+		MaxAge:   3600 * cookie_ttl,
+		HttpOnly: os.Getenv("GO_ENV") == "debug",
+		Secure:   os.Getenv("GO_ENV") != "debug",
+		SameSite: http.SameSiteLaxMode,
+	})
+	g.Use(sessions.Sessions("login", store))
+
+	srv := &http.Server{Addr: addr, Handler: g}
+	return g, srv
+}
+
+func RunServer(srv *http.Server, wg *sync.WaitGroup, ctx context.Context, log *logrus.Logger) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		log.Info("Starting server")
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.WithFields(logrus.Fields{
+				"err": err,
+			}).Error("Server error")
+		}
+	}()
+
+	go func() {
+		<-ctx.Done()
+
+		log.Info("Stopping server")
+
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(shutdownCtx); err != nil {
+			log.WithFields(logrus.Fields{
+				"err": err,
+			}).Error("Server graceful shutdown failed")
+		} else {
+			log.Info("Server stopped gracefully")
+		}
+	}()
+}
