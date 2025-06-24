bdea6ab7d6a8f18134de3eefb09cdf6bce54aabc
AsciiMoth <ascii@moth.contact>
2025-06-24 06:03:36 +0400

    Add cards management logic

--- handlers.go
+++ handlers.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"mime/multipart"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/gin-contrib/sessions"
@@ -85,7 +87,65 @@ func getUintParam(c *gin.Context, name string) (uint, error) {
 	return uint(iv), err
 }
 
-func SetupRoutes(g *gin.Engine, ctx context.Context, storage *BlobStorage, db *PGDB, log *logrus.Logger, providers []string) {
+func uploader(log *logrus.Logger, storage *BlobStorage, ctx context.Context, maxUploadSize int64) func(*gin.Context, *multipart.Form, string, string) bool {
+	return func(c *gin.Context, form *multipart.Form, input, key string) bool {
+		files, ok := form.File[input]
+		if ok && len(files) > 0 && files[0] != nil {
+			avatar := files[0]
+			if avatar.Size > maxUploadSize {
+				errorPage(c, http.StatusRequestEntityTooLarge, fmt.Sprintf("%s too large", avatar.Filename))
+				return false
+			}
+
+			mime := avatar.Header.Get("Content-Type")
+			if mime == "" {
+				errorPage(c, http.StatusBadRequest, fmt.Sprintf("Content type of %s unknown", avatar.Filename))
+			}
+
+			src, err := avatar.Open()
+			if err != nil {
+				log.WithFields(logrus.Fields{
+					"err": err,
+				}).Error("Failed to receive form file")
+				errorPage(c, http.StatusBadRequest, fmt.Sprintf("Broken file %s", avatar.Filename))
+				return false
+			}
+
+			defer src.Close()
+
+			err = storage.WriteKey(ctx, key, src, avatar.Size, mime)
+			log.WithFields(logrus.Fields{
+				"key": key,
+			}).Debug("File uploaded")
+
+			if err != nil {
+				log.WithFields(logrus.Fields{
+					"err": err,
+				}).Error("Failed to uload file to storage")
+				errorPage(c, http.StatusInternalServerError, fmt.Sprintf("Failed to upload file %s", avatar.Filename))
+				return false
+			}
+		}
+		return true
+	}
+}
+
+func SetupRoutes(
+	g *gin.Engine,
+	ctx context.Context,
+	storage *BlobStorage,
+	db *PGDB,
+	log *logrus.Logger,
+	providers []string,
+) {
+	max_upload_size := os.Getenv("MAX_UPLOAD_SIZE")
+	mus, err := strconv.ParseInt(max_upload_size, 10, 64)
+	if err != nil {
+		log.Fatalf("Failed to conf max upload size: %s", max_upload_size)
+	}
+
+	uploadFormFile := uploader(log, storage, ctx, mus)
+
 	g.Use(sessionMiddleware(log, db))
 
 	g.NoRoute(func(c *gin.Context) {
@@ -228,6 +288,189 @@ func SetupRoutes(g *gin.Engine, ctx context.Context, storage *BlobStorage, db *P
 			sess.Clear()
 			sess.Save()
 			c.Redirect(http.StatusTemporaryRedirect, "/")
+		})
+		authorized.GET("/cards", func(c *gin.Context) {
+			user := getUser(c)
+
+			cards, err := db.ListCards(user.ID)
+
+			if err != nil {
+				log.WithFields(logrus.Fields{
+					"err": err,
+				}).Error("Failed to list cards")
+				errorPage(c, http.StatusInternalServerError, "Failed to list cards")
+				return
+			}
+
+			c.HTML(http.StatusOK, "cards.html", gin.H{
+				"Title": "Your cards",
+				"User":  user,
+				"Cards": cards,
+			})
+		})
+		authorized.GET("/delcard/:id", func(c *gin.Context) {
+			user := getUser(c)
+
+			cid, err := getUintParam(c, "id")
+
+			if err != nil {
+				c.Redirect(http.StatusTemporaryRedirect, "/cards")
+				return
+			}
+
+			card, err := db.GetCard(cid)
+
+			if err != nil {
+				log.WithFields(logrus.Fields{
+					"cid": cid,
+					"err": err,
+				}).Error("Failed to find a card")
+				c.Redirect(http.StatusTemporaryRedirect, "/cards")
+				return
+			}
+
+			if card.Owner != user.ID {
+				errorPage(c, http.StatusForbidden, "Card is owned by another user")
+				return
+			}
+
+			err = db.DeleteCard(cid)
+
+			if err != nil {
+				log.WithFields(logrus.Fields{
+					"cid": cid,
+					"err": err,
+				}).Error("Failed to delete a card")
+			}
+
+			c.Redirect(http.StatusTemporaryRedirect, "/cards")
+		})
+		authorized.GET("/editor", func(c *gin.Context) {
+			c.HTML(http.StatusOK, "editor.html", gin.H{
+				"Title":        "Create new card",
+				"User":         getUser(c),
+				"EditUrl":      "/new",
+				"SubmitButton": "Create Card",
+			})
+		})
+		authorized.GET("/editor/:id", func(c *gin.Context) {
+			user := getUser(c)
+
+			cid, err := getUintParam(c, "id")
+
+			if err != nil {
+				c.Redirect(http.StatusTemporaryRedirect, "/cards")
+				return
+			}
+
+			card, err := db.GetCard(cid)
+			if err != nil {
+				c.Redirect(http.StatusTemporaryRedirect, "/cards")
+				return
+			}
+
+			c.HTML(http.StatusOK, "editor.html", gin.H{
+				"Title":        "Edit card",
+				"EditUrl":      fmt.Sprintf("/update/%d", cid),
+				"User":         user,
+				"SubmitButton": "Update Card",
+				"Card":         card,
+			})
+		})
+		authorized.POST("/new", func(c *gin.Context) {
+			user := getUser(c)
+
+			var fields CardFields
+
+			if err := c.Bind(&fields); err != nil {
+				log.WithFields(logrus.Fields{
+					"err": err,
+				}).Error("Failed to bind form data")
+				errorPage(c, http.StatusBadRequest, "Invalid form data")
+				return
+			}
+
+			form, err := c.MultipartForm()
+			if err != nil {
+				log.WithFields(logrus.Fields{
+					"err": err,
+				}).Error("Failed to get multipart form data")
+				errorPage(c, http.StatusBadRequest, "Invalid form data")
+				return
+			}
+
+			cardId, err := db.CreateCard(user.ID, fields)
+
+			if !uploadFormFile(c, form, "avatar", fmt.Sprintf("media/avatar/%d", cardId)) {
+				return
+			}
+
+			if !uploadFormFile(c, form, "logo", fmt.Sprintf("media/logo/%d", cardId)) {
+				return
+			}
+
+			c.Redirect(http.StatusFound, "/cards")
+		})
+		authorized.POST("/update/:id", func(c *gin.Context) {
+			user := getUser(c)
+
+			cid, err := getUintParam(c, "id")
+
+			if err != nil {
+				c.Redirect(http.StatusTemporaryRedirect, "/cards")
+				return
+			}
+
+			card, err := db.GetCard(cid)
+			if err != nil {
+				c.Redirect(http.StatusTemporaryRedirect, "/cards")
+				return
+			}
+
+			if card.Owner != user.ID {
+				c.Redirect(http.StatusTemporaryRedirect, "/cards")
+				return
+			}
+
+			var fields CardFields
+
+			if err := c.Bind(&fields); err != nil {
+				log.WithFields(logrus.Fields{
+					"err": err,
+				}).Error("Failed to bind form data")
+				errorPage(c, http.StatusBadRequest, "Invalid form data")
+				return
+			}
+
+			form, err := c.MultipartForm()
+			if err != nil {
+				log.WithFields(logrus.Fields{
+					"err": err,
+				}).Error("Failed to get multipart form data")
+				errorPage(c, http.StatusBadRequest, "Invalid form data")
+				return
+			}
+
+			card.Fields = fields
+			err = db.UpdateCard(card)
+
+			if err != nil {
+				log.WithFields(logrus.Fields{
+					"err": err,
+				}).Error("Failed to update the card")
+				errorPage(c, http.StatusBadRequest, "Failed to update card content")
+				return
+			}
+
+			if !uploadFormFile(c, form, "avatar", fmt.Sprintf("media/avatar/%d", cid)) {
+				return
+			}
+
+			if !uploadFormFile(c, form, "logo", fmt.Sprintf("media/logo/%d", cid)) {
+				return
+			}
+
+			c.Redirect(http.StatusFound, "/cards")
 		})
 	}
 }
